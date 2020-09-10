14cc6bcfb00ad5c125603f86ae3f394fc5480896
Sunny Mittal <smittal@truecar.com>
2020-09-10 12:45:20 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(title: 'Learn Relay', body: 'Learn Relay with Rails GraphQL backend')
+Todo.create(title: 'Learn Nx', body: 'Learn NX for module management')
+
+l1 = List.create
+l2 = List.create
+l3 = List.create
+
+l1.todos = Todo.all
+l2.todos << Todo.first
+l3.todos << Todo.last
